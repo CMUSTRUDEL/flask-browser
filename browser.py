63032f9fe1d6a851876e81b454509ae9c81c27e5
Bogdan Vasilescu <vasilescu@gmail.com>
2020-06-07 15:08:48 -0400

    tdg updates

--- browser.py
+++ browser.py
@@ -2,5 +2,10 @@
 # pip freeze > requirements.txt
 # flask run
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from app import app
 
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=3000)
